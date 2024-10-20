c8a2ad15bb398ae0be0b875ef4bfd46956cf07d1
Ivan Gjorcev <gjorcev.ivan@gmail.com>
2024-10-21 00:18:17 +0200

    Update

--- AuthApp/frontend/vite.config.js
+++ AuthApp/frontend/vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: true, // Enable minification for production
+    sourcemap: false, // Disable sourcemaps for production (optional)
+  },
   server: {
     proxy: {
-      '/api': 'https://authapp-server-user-login-crud.onrender.com'
+      '/api': 'https://authapp-server-user-login-crud.onrender.com' // Only for development
     }
-  },
-  build: {
-    minify: false,
-    sourcemap: true
   }
 });
 
