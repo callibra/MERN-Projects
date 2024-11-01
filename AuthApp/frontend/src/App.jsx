e3b024dfa779a4ed954b3811b8abd3a7f28e5c0d
Ivan Gjorcev <gjorcev.ivan@gmail.com>
2024-11-01 20:02:57 +0100

    Update

--- AuthApp/frontend/src/App.jsx
+++ AuthApp/frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
         <WorkoutsContextProvider>
           <div className="pages">
             <Routes>
-              <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+              <Route path="/" element={user ? <Home /> : <Navigate to="/" />} />
               <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
               <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
             </Routes>
