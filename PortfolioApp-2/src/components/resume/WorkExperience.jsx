b2ee5efaff117b924c5a4f4e8348b86b228e0ed2
Ivan Gjorcev <gjorcev.ivan@gmail.com>
2024-11-03 20:43:56 +0100

    Update

--- PortfolioApp-2/src/components/resume/WorkExperience.jsx
+++ PortfolioApp-2/src/components/resume/WorkExperience.jsx
@@ -5,7 +5,7 @@ const WorkExperience = [
             company: "Tech Innovations",
             yearsActive: "March 2021 - Present",
             link: "https://images.ctfassets.net/h6luvadnbip0/5QEHRZ6WOuekM8MRnGNH69/10dd1322b39aa925cc6b1d63e892eb1d/Gruppe_-25.png", 
-            image: "/public/project.png", 
+            image: "../../assets/project.png", 
             information: [
                 "Developed and maintained web applications using React and Node.js.",
                 "Collaborated with cross-functional teams to define and design new features.",
@@ -18,7 +18,7 @@ const WorkExperience = [
             company: "Creative Solutions",
             yearsActive: "June 2019 - February 2021",
             link: "https://images.ctfassets.net/h6luvadnbip0/5QEHRZ6WOuekM8MRnGNH69/10dd1322b39aa925cc6b1d63e892eb1d/Gruppe_-25.png", 
-            image: "/public/project.png", 
+            image: "../../assets/project.png", 
             information: [
                 "Designed and implemented user-friendly web interfaces with a focus on UX/UI.",
                 "Worked closely with designers to ensure high-quality visuals and interactions.",
@@ -31,7 +31,7 @@ const WorkExperience = [
             company: "Innovative Apps",
             yearsActive: "May 2018 - August 2018",
             link: "https://images.ctfassets.net/h6luvadnbip0/5QEHRZ6WOuekM8MRnGNH69/10dd1322b39aa925cc6b1d63e892eb1d/Gruppe_-25.png",
-            image: "/public/project.png", 
+            image: "../../assets/project.png", 
             information: [
                 "Assisted in the development of a mobile application for tracking fitness goals.",
                 "Participated in daily stand-ups and contributed to team meetings.",
@@ -44,7 +44,7 @@ const WorkExperience = [
             company: "Global Tech Services",
             yearsActive: "September 2016 - April 2018",
             link: "https://images.ctfassets.net/h6luvadnbip0/5QEHRZ6WOuekM8MRnGNH69/10dd1322b39aa925cc6b1d63e892eb1d/Gruppe_-25.png", 
-            image: "/public/project.png", 
+            image: "../../assets/project.png", 
             information: [
                 "Provided technical support for software and hardware issues to clients.",
                 "Documented and resolved customer inquiries and troubleshooting steps.",
@@ -57,7 +57,7 @@ const WorkExperience = [
             company: "NextGen Solutions",
             yearsActive: "January 2015 - August 2016",
             link: "https://images.ctfassets.net/h6luvadnbip0/5QEHRZ6WOuekM8MRnGNH69/10dd1322b39aa925cc6b1d63e892eb1d/Gruppe_-25.png", 
-            image: "/public/project.png", 
+            image: "../../assets/project.png", 
             information: [
                 "Coordinated project schedules and resources for multiple software development projects.",
                 "Communicated with stakeholders to gather requirements and provide project updates.",
