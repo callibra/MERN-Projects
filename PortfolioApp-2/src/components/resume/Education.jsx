0c0bc9fefd232c657822b930eb4d817cd224e626
Ivan Gjorcev <gjorcev.ivan@gmail.com>
2024-11-03 20:53:26 +0100

    Update

--- PortfolioApp-2/src/components/resume/Education.jsx
+++ PortfolioApp-2/src/components/resume/Education.jsx
@@ -5,7 +5,7 @@ const Education = [
         degree: "Bachelor of Arts in History",
         yearsAttended: "2010 - 2014",
         link: "https://images.ctfassets.net/h6luvadnbip0/5QEHRZ6WOuekM8MRnGNH69/10dd1322b39aa925cc6b1d63e892eb1d/Gruppe_-25.png",
-        image: "../../assets/project.png", 
+        image: "project.png", 
         details: [
             "Graduated with High Distinction",
             "Relevant Coursework: American History, World History, Political Science",
@@ -17,7 +17,7 @@ const Education = [
         degree: "Master of Science in Computer Science",
         yearsAttended: "2015 - 2017",
         link: "https://images.ctfassets.net/h6luvadnbip0/5QEHRZ6WOuekM8MRnGNH69/10dd1322b39aa925cc6b1d63e892eb1d/Gruppe_-25.png",
-        image: "../../assets/project.png", 
+        image: "project.png", 
         details: [
             "Focused on Artificial Intelligence and Machine Learning",
             "Thesis: 'Deep Learning Techniques for Image Recognition'",
@@ -29,7 +29,7 @@ const Education = [
         degree: "Bachelor of Science in Electrical Engineering",
         yearsAttended: "2011 - 2015",
         link: "https://images.ctfassets.net/h6luvadnbip0/5QEHRZ6WOuekM8MRnGNH69/10dd1322b39aa925cc6b1d63e892eb1d/Gruppe_-25.png",
-        image: "../../assets/project.png", 
+        image: "project.png", 
         details: [
             "Completed an undergraduate research project on Robotics",
             "Interned at a leading tech company in the summer of 2014",
@@ -41,7 +41,7 @@ const Education = [
         degree: "Ph.D. in Chemistry",
         yearsAttended: "2016 - 2021",
         link: "https://images.ctfassets.net/h6luvadnbip0/5QEHRZ6WOuekM8MRnGNH69/10dd1322b39aa925cc6b1d63e892eb1d/Gruppe_-25.png",
-        image: "../../assets/project.png", 
+        image: "project.png", 
         details: [
             "Conducted research on renewable energy sources",
             "Published papers in leading scientific journals",
@@ -53,7 +53,7 @@ const Education = [
         degree: "Bachelor of Arts in Philosophy, Politics, and Economics (PPE)",
         yearsAttended: "2009 - 2012",
         link: "https://images.ctfassets.net/h6luvadnbip0/5QEHRZ6WOuekM8MRnGNH69/10dd1322b39aa925cc6b1d63e892eb1d/Gruppe_-25.png",
-        image: "../../assets/project.png", 
+        image: "project.png", 
         details: [
             "Participated in student government and debate club",
             "Relevant Coursework: Ethics, Political Theory, Economic Principles",
