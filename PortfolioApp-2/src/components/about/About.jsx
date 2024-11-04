26c5d64f284ca79f39cd9595d63dc1f5cfafac54
Ivan Gjorcev <gjorcev.ivan@gmail.com>
2024-11-04 17:24:34 +0100

    Update

--- PortfolioApp-2/src/components/about/About.jsx
+++ PortfolioApp-2/src/components/about/About.jsx
@@ -26,10 +26,8 @@ const About = () => {
     return (
         <section className="about container section" id="about">
             <h2 className="section__title">About Me</h2>
-
             <div className="about__container grid">
                 <img src={Image} alt="" className='about__img' />
-
                 <div className="about__data grid">
                     <div className="about__info">
                         <p className="about__description">
@@ -40,80 +38,80 @@ const About = () => {
                         <h3 className='skills-title'>🛠️ Skills :</h3>
                         <ul className="about__list">
     <li>
-        <b>JavaScript</b> 
+        <b>JavaScript </b> 
         <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noopener noreferrer">
             <img src={jsIcon} alt="JavaScript" />
         </a>
     </li>
     <li>
-        <b>TypeScript</b> 
+        <b>TypeScript </b> 
         <a href="https://www.typescriptlang.org/" target="_blank" rel="noopener noreferrer">
             <img src={tsIcon} alt="TypeScript" />
         </a>
     </li>
     <li>
-        <b>React</b> 
+        <b>React </b> 
         <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
             <img src={reactIcon} alt="React" />
         </a>
     </li>
     <li>
-        <b>Node.js</b> 
+        <b>Node.js </b> 
         <a href="https://nodejs.org/" target="_blank" rel="noopener noreferrer">
             <img src={nodejsIcon} alt="Node.js" />
         </a>
     </li>
     <li>
-        <b>Python</b> 
+        <b>Python </b> 
         <a href="https://www.postgresql.org/" target="_blank" rel="noopener noreferrer">
             <img src={pythonIcon} alt="Postgres SQL" />
         </a>
     </li>
     <li>
-        <b>NestJS</b> 
+        <b>NestJS </b> 
         <a href="https://nestjs.com/" target="_blank" rel="noopener noreferrer">
             <img src={nestjsIcon} alt="NestJS" />
         </a>
     </li>
     <li>
-        <b>Express</b> 
+        <b>Express </b> 
         <a href="https://expressjs.com/" target="_blank" rel="noopener noreferrer">
             <img src={expressIcon} alt="Express" />
         </a>
     </li>
     <li>
-        <b>MongoDB</b> 
+        <b>MongoDB </b> 
         <a href="https://www.mongodb.com/" target="_blank" rel="noopener noreferrer">
             <img src={mongodbIcon} alt="MongoDB" />
         </a>
     </li>
     <li>
-        <b>Php</b> 
+        <b>Php </b> 
         <a href="https://www.php.net/" target="_blank" rel="noopener noreferrer">
             <img src={phpIcon} alt="PHP" />
         </a>
     </li>
     <li>
-        <b>Next.js</b> 
+        <b>Next.js </b> 
         <a href="https://nextjs.org/" target="_blank" rel="noopener noreferrer">
             <img src={nextjsIcon} alt="Next.js" />
         </a>
     </li>
     <li>
-        <b>GitHub</b> 
+        <b>GitHub </b> 
         <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
             <img src={githubIcon} alt="GitHub" />
         </a>
     </li>
     <li>
-        <b>WordPress</b> 
+        <b>WordPress </b> 
         <a href="https://wordpress.org/" target="_blank" rel="noopener noreferrer">
             <img src={wordpressIcon} alt="WordPress" />
         </a>
     </li>
 </ul>
                     </div>
-                    <hr className="line-hr" />
+                    {/* <hr className="line-hr" /> */}
                     <h3 className='skills-title'>📊 Graph :</h3>
                     <div className="about__skills grid">
                         <div className="skills__data">
@@ -145,10 +143,9 @@ const About = () => {
                                 <span className="skills__percentage photography">
                                 </span>
                             </div>
-                            <hr className="line-hr" />
-                        <button className="btn" onClick={downloadResume}>Download CV</button>
                         </div>
                     </div>
+                    <button className="cv-resume" onClick={downloadResume}>Download CV</button>
                 </div>
             </div>
             <AboutBox />
@@ -157,3 +154,5 @@ const About = () => {
 }
 
 export default About;
+
+///////////////////// 888 ////////////////////////////
